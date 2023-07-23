0b9eac7b72657a77befef309f81d934140ec42cf
Pietro Zardini <pizardini@gmail.com>
2023-07-23 13:41:02 -0300

    alterações no service para busca de api

--- src/app/pessoas/people.service.ts
+++ src/app/pessoas/people.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Person } from '../shared/models/Person.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
+  private serverUrl = 'http:localhost:300/people';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPeople(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.serverUrl)
+  }
+
+  postPerson() {
+
+  }
+
+  putPerson() {
+
+  }
+
+  patchPerson() {
+
+  }
+
+  deletePerson() {
+
+  }
 }
